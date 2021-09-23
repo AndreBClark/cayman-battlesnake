95ea62d72a2a475ce7935e6b4d0a4fde07411809
Andre <andre@cosmicdivision.dev>
2021-09-22 23:54:51 -0700

    convert express app to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
-const express = require('express')
-const { info, start, move, end } = require('./logic')
+import express, { Request, Response } from "express"
+
+import { info, start, move, end } from "./logic";
 
 const app = express()
 app.use(express.json())
 
 const port = process.env.PORT || 8080
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
     res.send(info())
 });
 
-app.post("/start", (req, res) => {
+app.post("/start", (req: Request, res: Response) => {
     res.send(start(req.body))
 });
 
-app.post("/move", (req, res) => {
+app.post("/move", (req: Request, res: Response) => {
     res.send(move(req.body))
 });
 
-app.post("/end", (req, res) => {
+app.post("/end", (req: Request, res: Response) => {
     res.send(end(req.body))
 });
 
 // Start the Express server
 app.listen(port, () => {
     console.log(`Starting Battlesnake Server at http://0.0.0.0:${port}...`)
-})
+});
